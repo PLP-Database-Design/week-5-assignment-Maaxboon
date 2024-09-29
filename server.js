7b13c4bbeff2bb3fdd8c74bbe0291823804eb987
Maaxboon <maaxwelokoth@gmail.com>
2024-09-29 12:50:41 +0300

    Adds a GET endpoint for retrievals

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
+const express = require('express');
 const mysql = require('mysql2');
 const dotenv = require('dotenv');
+const app = express();
 
 dotenv.config();
 
@@ -10,11 +12,62 @@ const db = mysql.createConnection({
   database: process.env.DB_NAME
 });
 
-// Test the database connection
 db.connect((err) => {
   if (err) {
     console.error('Database connection failed:', err.stack);
     return;
   }
   console.log('Connected to the database.');
+});
+
+// Retrieve all patients
+app.get('/patients', (req, res) => {
+  const query = 'SELECT patient_id, first_name, last_name, date_of_birth FROM patients';
+  db.query(query, (err, results) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    res.json(results);
+  });
+});
+
+// Retrieve all providers
+app.get('/providers', (req, res) => {
+  const query = 'SELECT first_name, last_name, provider_specialty FROM providers';
+  db.query(query, (err, results) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    res.json(results);
+  });
+});
+
+// Filter patients by First Name
+app.get('/patients/:firstName', (req, res) => {
+  const { firstName } = req.params;
+  const query = 'SELECT patient_id, first_name, last_name, date_of_birth FROM patients WHERE first_name = ?';
+  db.query(query, [firstName], (err, results) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    res.json(results);
+  });
+});
+
+// Retrieve providers by specialty
+app.get('/providers/specialty/:specialty', (req, res) => {
+  const { specialty } = req.params;
+  const query = 'SELECT first_name, last_name, provider_specialty FROM providers WHERE provider_specialty = ?';
+  db.query(query, [specialty], (err, results) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    res.json(results);
+  });
+});
+
+// listen to the server
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`server is running on http://localhost:${PORT}`);
 });
